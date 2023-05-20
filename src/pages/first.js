ad390ec5690824ec7fecfd7f494d870868528e8a
user941211 <common941211@gmail.com>
2023-05-20 14:05:22 +0900

    기본 틀 생성

---
+++ src/pages/first.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Movie from '../components/movie';
+import '.css/first.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function first(){
+    return(
+        <div>
+            <button classname="enter">시작하기</button>
+        </div>
+    );
+}
+export default first;
